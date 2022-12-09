8fa89ca0af726c994ed743453ee7cf0fdecc20be
janetuyy <tyutyunnikowa.jane@yandex.ru>
2022-12-09 06:22:45 +0300

    cat2.4

--- lesson2/task4/task4.py
+++ lesson2/task4/task4.py
@@ -2,4 +2,14 @@
 # Найдите произведение элементов на указанных позициях.
 # Позиции хранятся в файле file.txt в одной строке одно число.
 
-print('n=')
+n = int(input('n='))
+s=[]
+res=1
+for i in range(-n, n+1):
+    s.append(i)
+with open('file.txt', 'r') as f:
+    positions = f.readlines()
+for j in positions:
+    k=int(j)
+    res*=s[k]
+print(res)
